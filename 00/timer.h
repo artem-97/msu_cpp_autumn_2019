1d303f53bfba58ee28fdec2f0560c4b46e310aa2
Artem Bodunov <artembodunov@mail.ru>
2019-10-10 13:57:05 +0300

    0 hw

---
+++ 00/timer.h
@@ -0,0 +1,18 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <iostream>
+#include <chrono>
+
+class Timer{
+	using clock_t = std::chrono::high_resolution_clock;
+	using microseconds = std::chrono::microseconds;
+
+	const clock_t::time_point start_;
+
+public:
+	Timer();
+	~Timer();
+};
+
+#endif
